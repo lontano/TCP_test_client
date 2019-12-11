dbb71ec3ebf98b26c6a07926ef37816444fb0b81
Angel Andres <gelo_oleg@hotmail.com>
2019-12-11 14:08:53 +0100

    Advanced RT Head simulator

---
+++ SerialComTest/SerialComTest/WindowsApp1/Connections/SerialReceiver.vb
@@ -0,0 +1,36 @@
+﻿Imports System.ComponentModel
+Imports System.IO.Ports
+
+Namespace Connections
+  Public Class SerialReceiver
+    Private _comPortName As String
+    Private WithEvents _comPort As IO.Ports.SerialPort
+
+    Public Event ErrorEvent(ByRef sender As SerialReceiver, ByVal CiException As Exception)
+    Public Event DataReceive(ByRef sender As SerialReceiver, ByVal siData As String)
+    Public Event DataReceiveBytes(ByRef sender As SerialReceiver, ByRef biData() As Byte)
+    Public Event ActivityIncoming(ByRef sender As SerialReceiver)
+
+    Public Function Listen(ByVal siComPort As String) As Boolean
+      If My.Computer.Ports.SerialPortNames.Contains(siComPort) Then
+        _comPortName = siComPort
+        _comPort = My.Computer.Ports.OpenSerialPort(_comPortName, 38400)
+        _comPort.BaudRate = 38400
+      Else
+        _comPortName = ""
+      End If
+      Return (_comPortName <> "")
+    End Function
+
+    Public Sub Disconnect()
+      _comPort.Close()
+      _comPort = Nothing
+    End Sub
+
+    Private Sub _comPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles _comPort.DataReceived
+      Threading.Thread.Sleep(5)
+      RaiseEvent DataReceive(Me, _comPort.ReadExisting())
+    End Sub
+  End Class
+
+End Namespace
--- SerialComTest/SerialComTest/WindowsApp1/RTHead/RTHeadPacket.vb
+++ SerialComTest/SerialComTest/WindowsApp1/RTHead/RTHeadPacket.vb
@@ -1,9 +1,14 @@
-﻿Public Class RTHeadProtocol
+﻿Public Class RTHeadPacket
   Public Property Pan As Integer = 0
   Public Property Tilt As Integer = 0
   Public Property Zoom As Integer = 0
   Public Property Focus As Integer = 0
   Public Property Index As Integer = 0
+  Public Property TimeStamp As Date = Now
+
+  Public Shared ReadOnly Property StartingByte As Byte = 4
+  Public Shared ReadOnly Property ByteCount As Integer = 15
+  Public Shared ReadOnly Property PacketTime As Double = 10
 
   Private _crc As Integer = 0
   Public ReadOnly Property CRC As Integer
@@ -11,6 +16,21 @@
       Return _crc
     End Get
   End Property
+
+  Public Shared Function ComputeCRC(data() As Byte) As Integer
+    Dim crc As Integer = 0
+    Try
+      crc = 0
+      If data.Length >= RTHeadPacket.ByteCount - 2 Then
+        For i As Integer = 0 To RTHeadPacket.ByteCount - 2
+          crc += data(i)
+        Next
+      End If
+      crc = crc Mod 256
+    Catch ex As Exception
+    End Try
+    Return crc
+  End Function
 
   Public Property Bytes() As Byte()
     Get
@@ -41,7 +61,7 @@
       Return myBytes.ToArray
     End Get
     Set(value As Byte())
-      If value.Length = 15 AndAlso value(0) = 4 Then
+      If value.Length >= 15 AndAlso value(0) = 4 Then
         Me.Index = value(1) Mod 128
 
         Dim myBytes As New List(Of Byte)
@@ -64,4 +84,34 @@
     End Set
   End Property
 
+  Public Sub New()
+
+  End Sub
+
+  Public Sub New(data() As Byte)
+    Me.Bytes = data
+  End Sub
+
+  Public Shared Function GetPacket(data() As Byte) As RTHeadPacket
+    Try
+      'do we have enough data for a packet?
+      If data.Length < RTHeadPacket.ByteCount Then Return Nothing
+      'is the first byte correct?
+      If data(0) <> RTHeadPacket.StartingByte Then Return Nothing
+
+      Dim aux As New RTHeadPacket(data)
+      'is the CRC right?
+      If data(RTHeadPacket.ByteCount - 1) = RTHeadPacket.ComputeCRC(data) Then
+        Return aux
+      Else
+        Return Nothing
+      End If
+    Catch ex As Exception
+      Return Nothing
+    End Try
+  End Function
+
+  Public Overrides Function ToString() As String
+    Return MyBase.ToString & " " & Me.Index & " pan=" & Me.Pan & " tilt=" & Me.Tilt & " zoom=" & Me.Zoom & " focus=" & Me.Focus & " timeStamp=" & Me.TimeStamp.ToUniversalTime & "." & Strings.Format(Me.TimeStamp.Millisecond, "000")
+  End Function
 End Class
